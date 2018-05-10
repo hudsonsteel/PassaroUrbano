086fdf5bb4855b2e9436428fea38e11576a65e67
Hudson Steel TI <hudsons@mgcontecnica.com.br>
2018-05-10 15:42:13 -0300

    Atualizado Link API Fake

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,3 @@
-export const URL_API = 'http://localhost:3000'
+// export const URL_API = 'http://localhost:3000'
+export const URL_API = 'https://my-json-server.typicode.com/hudsonsteel/PassaroUrbano'
+//https://my-json-server.typicode.com/hudsonsteel/PassaroUrbano
